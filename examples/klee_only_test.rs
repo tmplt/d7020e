f6a7dad85ec0c940990470e0c15dd2080ef5289e
Per <Per Lindgren>
2019-12-30 17:31:41 +0100

    klee_only_test

--- examples/klee_only_test.rs
+++ examples/klee_only_test.rs
@@ -10,10 +10,13 @@ use panic_klee as _;
 fn main() {
     let mut a = 0;
     klee_make_symbolic!(&mut a, "a");
-    match a {
+    // Rust panic on a == 200;
+    let _ = 100 / (a - 200);
+
+    let _ = match a {
         0 => klee_abort!(),
         1 => klee_abort!(),
-        2 => panic!(),
+        2 => klee_abort!(),
         3 => panic!("3"), // just one instance of panic! will be spotted
         4 => klee_assert!(false),
         5 => klee_assert_eq!(false, true),
